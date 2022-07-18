23d11900b6b2d69de1f515a1cb649f27e1c40ef3
Vilson1984 <vcmorion@hotmail.com>
2022-07-18 20:13:46 -0300

    update list automatically

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import './App.css';
 import {useState, useEffect} from 'react';
-import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs";
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill, BsFillPatchCheckFill} from "react-icons/bs";
 
 const API = "http://localhost:5000";
 
-function App() {
+function App() {                                                                                                      
   const [title, setTitle] = useState ("") //consultar título e altera o valor, começando com valor nulo
   const [time, setTime] = useState ("")
   const [todos, setTodos] = useState([]) // A lista começa vazia, p podermos inserir valores nela
   const [loading, setLoading] = useState (false); // false, pois ao começar começa vazia, e ao carregar os dados acionará o loading
   
-  const handleSubmit = (e) => { // (e) é o evento em si. Com ele permito não recarregar a pg no envio do formulário
-    e.preventDefault(); // com esse método consigo parar o envio do form
-   
+  //A primeira ação é ler o const todo na hora do carregamento
+  useEffect(() => {
+
+    const loadData = async() =>{
+      setLoading(true)
+
+      const res = await fetch(API + "/todos")
+      .then((res) => res.json()) //Espera uma resposta e transforma-a em um json
+      .then((data) => data)      //Pega a resposta e coloca num array de objetos
+      .catch((err) => console.log(err));// informa o erro ocorrido
+                                                                                                                                          
+    setLoading (false); //a função await é até carregar as 3 funções. Feito isso, nãao precisa mais carregar nada, ou seja , o loading é falso.
+
+    setTodos (res); //Resposta transformada num array de objetos
+    }
+
+    loadData();
+  }, [])
+
+  const handleSubmit = async (e) => { // (e) é o evento em si. Com ele permito não recarregar a pg no envio do formulário
+
+    e.preventDefault(); // com esse método consigo parar o envio do form   
     const todo = {
       id: Math.random(), //cria um id aleatório
       title, // obj com mesmo nome, então preenche com valor de title
       time,
       done:false, // tarefa entra como não completa no sistema
     };
+    
+    //ENVIANDO DADOS P/ API
+    await fetch(API + "/todos",{
+      method: "POST", ///POst está inserindo dados
+      body: JSON.stringify(todo),//Converte todo o obj "const todo" em uma string e manda p/ back end trabalhar
+      headers:{
+        "Content-Type": "application/json",
+      },
+    });
 
-    console.log(todo) // Envio da tarefa para Backend, para API
+    setTodos((prevState) => [...prevState, todo]);
 
     setTitle(""); // quando envia, limpa o formulário
     setTime("");
@@ -80,6 +108,12 @@ function App() {
       <div className='list-to_do'>
         <h2>Lista de Tarefas:</h2>
         {todos.length === 0 && <p>Não há tarefas pendentes!</p>} 
+        {todos.map((todos) =>(
+          <div className='todo' key={todos.id}>
+            {todos.title}
+
+          </div>
+        ))}
       </div>
     </div>
   );
